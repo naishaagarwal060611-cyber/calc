79da80e5cb97c1b5d6e7aaa290dfdc86978fbadc
Naisha <naishaagarwal060611@gmail.com>
2026-06-10 17:25:13 +0530

    first commit

---
+++ prog.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+c = int(input("Enter a third number: "))
+
+def yes():
+    print(a + b + c)
+    print(a * b - c)
+    print(a - c)
+
+
+yes()
